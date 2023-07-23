880e6b194fc3af6e16e405f5f7592bdf6088f585
María Nelo <maria.nelo.dam@gmail.com>
2023-07-23 03:06:57 +0200

    Desarrollo Ejercicio1

---
+++ Ejercicio1.js
@@ -0,0 +1,14 @@
+// Creamos el objeto 'persona'
+
+let persona= {
+
+    nombre : 'María',
+    apellido : 'Nelo',
+    bootcamp : ['Git & Github', 'html y css', 'JavaScript'],
+    busqueda_activa : true,
+    rrss : {
+        nombre : 'Twitter',
+        enlace : 'https://twitter.com/mariaalenelobaz',
+    }
+};
+
