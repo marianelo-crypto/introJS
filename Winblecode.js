561746c4d2866f30cde22148c1fc7009ea277277
María Nelo <maria.nelo.dam@gmail.com>
2023-08-13 16:36:54 +0200

    He armado parte del programa, pero he invertido juegos y rondas.

--- Winblecode.js
+++ Winblecode.js
@@ -1,86 +1,102 @@
+// Creamos el objeto jugador
 const jugador = {
-    gamer: [
-        {
-            nombre: "",
-            numero: 0,
-            puntos: 0,
-        },
-        {
-            nombre: "",
-            numero: 1,
-            puntos: 0,
-        },
-        {
-            nombre: "",
-            numero: 2,
-            puntos: 0,
-        },
-    ],
+  // Creamos el array de objetos gamer
+  gamer: [
+    {
+      nombre: "", // Nombre del jugador 0
+      numero: 0, // Número del jugador 0
+      puntos: 0, // Puntos del jugador 0
+      juegos: 0,
+      rondas:0,
+    },
+    {
+      nombre: "", // Nombre del jugador 1
+      numero: 1, // Número del jugador 1
+      puntos: 0, // Puntos del jugador 1
+    },
+    {
+      nombre: "", // Nombre del jugador 2
+      numero: 2, // Número del jugador 2
+      puntos: 0, // Puntos del jugador 2
+      juegos: 0,
+      rondas:0,
+    },
+  ],
 
-    // Constructor de partida
-    createMatch: function (nom1, nom2) {
-        // Variables de jugadores
-        this.gamer[1].nombre = nom1;
-        this.gamer[2].nombre = nom2;
+  // Constructor de partida
+  createMatch: function (nom1, nom2) {
+    // Variables de jugadores
+    this.gamer[1].nombre = nom1; // Asignar nombre al jugador 1
+    this.gamer[2].nombre = nom2; // Asignar nombre al jugador 2
+   
 
-        const self = this; // Guardar la referencia a "this"
+    const self = this; // Guardar la referencia a "this"
 
-        // Función para asignar puntos
-        function pointWonBy(jugadorNum) {
-            let jugador_otro = jugadorNum === 1 ? 2 : 1;
+    function pointWonBy(jugadorNum) {
+      let jugador_otro = jugadorNum === 1 ? 2 : 1; // Obtener el número del otro jugador
 
-            switch (self.gamer[jugadorNum].puntos) {
-                case 0:
-                    self.gamer[jugadorNum].puntos = 15;
-                    break;
-                case 15:
-                    self.gamer[jugadorNum].puntos = 30;
-                    break;
-                case 30:
-                    if (self.gamer[jugador_otro].puntos === 40) {
-                        self.gamer[jugadorNum].puntos = 40;
-                    } else {
-                        self.gamer[jugadorNum].puntos = 40;
-                    }
-                    break;
-                case 40:
-                    if (self.gamer[jugador_otro].puntos === 40) {
-                        self.gamer[jugadorNum].puntos = 45;
-                    } else if (self.gamer[jugador_otro].puntos === 45) {
-                        self.gamer[jugador_otro].puntos = 40;
-                    }
-                    break;
-                case 45:
-                    if (self.gamer[jugador_otro].puntos === 40) {
-                        self.gamer[jugadorNum].puntos = "Gana";
-                        
-                    }
-                    break;
-            }
-        }
+      switch (self.gamer[jugadorNum].puntos) {
+        case 0:
+          self.gamer[jugadorNum].puntos = 15; // Puntuación de 15
 
-        function getCurrentRoundScore() {
-            if(self.gamer[1].puntos===40 && self.gamer[2].puntos===40) {
-                console.log("Deuce");
-            } else if (self.gamer[1].puntos===45){
-                console.log("Advantage " + self.gamer[1].nombre);    
-            }else if (self.gamer[2].puntos===45){
-                console.log("Advantage " + self.gamer[2].nombre);    
-            }else{
-            console.log(
-                self.gamer[1].nombre +
-                " " +
-                self.gamer[1].puntos +
-                " - " +
-                self.gamer[2].puntos +
-                " " +
-                self.gamer[2].nombre
-            );
-        }
+          break;
+        case 15:
+          self.gamer[jugadorNum].puntos = 30; // Puntuación de 30
+
+          break;
+        case 30:
+          if (self.gamer[jugador_otro].puntos === 40) {
+            self.gamer[jugadorNum].puntos = 40; // Puntuación de 40
+          } else {
+            self.gamer[jugadorNum].puntos = 40; // Puntuación de 40 (repetida)
+          }
+          break;
+        case 40:
+          if (self.gamer[jugador_otro].puntos === 40) {
+            self.gamer[jugadorNum].puntos = 45; // Puntuación de 45 (ventaja)
+          } else if (self.gamer[jugador_otro].puntos === 45) {
+            self.gamer[jugador_otro].puntos = 40; // Cambiar la puntuación del otro jugador a 40
+          } else {
+            self.gamer[jugadorNum].puntos = "Gana";
+          }
+
+          break;
+        case 45:
+          self.gamer[jugadorNum].puntos = "Gana"; // Jugador gana el juego
+
+          break;
+      }
     }
-        return {
-            pointWonBy: pointWonBy,
-            getCurrentRoundScore: getCurrentRoundScore
-        };
+
+    function getCurrentRoundScore() {
+      if (self.gamer[1].puntos === 40 && self.gamer[2].puntos === 40) {
+        console.log("Deuce"); // Puntuación en empate (Deuce)
+      } else if (self.gamer[1].puntos === 45) {
+        console.log("Advantage " + self.gamer[1].nombre); // Ventaja para jugador 1
+      } else if (self.gamer[2].puntos === 45) {
+        console.log("Advantage " + self.gamer[2].nombre); // Ventaja para jugador 2
+      } else {
+        console.log(
+          self.gamer[1].nombre +
+            " " +
+            self.gamer[1].puntos +
+            " - " +
+            self.gamer[2].puntos +
+            " " +
+            self.gamer[2].nombre
+        ); // Mostrar puntuación normal
+      }
     }
-}
+    function getMatchScore() {
+      
+
+      
+    }
+
+    return {
+      pointWonBy: pointWonBy, // Devolver la función para asignar puntos
+      getCurrentRoundScore: getCurrentRoundScore, // Devolver la función para obtener la puntuación actual
+      getMatchScore: getMatchScore,
+    };
+  },
+};
